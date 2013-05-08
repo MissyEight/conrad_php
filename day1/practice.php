f02a3cbe0ce9a8f59e42aedf4a464f5bd0d8f9cb
Melissa Conrad <melissa.conrad209@gmail.com>
2013-05-08 11:55:53 -0700

    adding success and error messages

--- day1/practice.php
+++ day1/practice.php
@@ -1,9 +1,33 @@
+<?php
+// create some vars - possible values are 'success' or 'error'
+$status ='success';
+
+// logic to contol the message text based on the status
+if( $status == 'success'){
+	$message = 'Your information was submitted successfully! YAY!';
+}else{
+	$message = 'Sorry. Something went wrong. Try again.';
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
+	<style>
+		.error {
+			background-color: #F00;
+		}
+		.success {
+			background-color: #0f0;
+		}
+	</style>
 </head>
 
 <body>
-	<?php echo 'Hello World'; ?>
+	<div class="<?php echo $status; ?>">
+		<?php
+		// this is a secret comment
+		echo $message; ?>
+	</div>
 </body>
 </html>
