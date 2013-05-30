fbd94f0ed56671cbcd25b892a9beecfea1265d7c
Melissa Conrad <melissa.conrad209@gmail.com>
2013-05-30 11:57:03 -0700

    Create admin pg, login and register forms w validation

---
+++ blog/login.php
@@ -0,0 +1,117 @@
+<?php 
+// open a new session or resume the existing session
+session_start();
+require('db_connect.php');
+include_once('functions.php');
+
+//if the user is already logged in, push them to their admin panel
+if($_SESSION['logged_in']){
+	header('Location:admin.php');
+}
+
+
+// if the form was submitted, try to log them in by using post to get value
+if( $_POST['did_login'] == 1 ){
+	// extract the values the user typed in (sanitize)
+	$orig_username = $_POST['username'];
+	$orig_password = $_POST['password'];
+	$clean_username = clean_input($_POST['username'], $db); //set variable to easier refer to this code, trim trims white space off of user name, strip_tags strips html/php tags entered by the user
+	$clean_password = clean_input($_POST['password'], $db); // $password = strip_tags(trim($_POST['password']));
+
+	//apply a hash to the password
+	$sha_password = sha1($clean_password);
+
+	// ck to see if minimum lengths are met (validate)
+	if( strlen( $clean_username ) >= 5 AND strlen($clean_password) >= 5 ){ // cks if username & password is at least 5 characters
+
+		//Look for a user that matches in the database
+		$query = "SELECT user_id
+					FROM users
+					WHERE username = '$clean_username'
+					AND password = '$sha_password' 
+					AND is_admin = 1
+					LIMIT 1";
+		$result = $db->query($query);
+
+
+		// if one record is found, log them in
+		if ( 1 == $result->num_rows  ) {
+			// use cookies and sessions to remember the user
+			$_SESSION['logged_in'] = 1;
+			setcookie( 'logged_in', 1, time() + 60 * 60 * 24 * 14); // session and cookie logged_in are 2 different things (name, value, when expires seconds*minutes*hrs*days(=2weeks))
+			//direct the now logged-in user to the admin panel
+			header( 'Location:admin.php' );
+		}else{
+			$error = 1;
+		}
+	}else{
+		// username or pass too short
+		$error = 1;	
+	}
+}
+// if the user is trying to log out, unset and destroy the session and cookies
+if( $_GET['action'] == 'logout'){
+	unset( $_SESSION['logged_in'] );
+	session_destroy();
+	setcookie('logged_in', '', time() - 60 * 60 * 24 * 365 ); // cookie = '' still exists so we set it back 1 year to expire
+}
+// if the user visits the page and the cookie is still valid, re-create the session variable
+elseif( $_COOKIE['logged_in'] == 1 ) {
+	$_SESSION['logged_in'] = 1;
+}
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Log in to your account</title>
+<link href="css/reset.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="css/forms.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href='http://fonts.googleapis.com/css?family=Days+One' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<div id="wrapper">
+<div id="container">	
+<?php 
+// if not logged in, show the form
+if( !$_SESSION['logged_in'] ){ ?>
+
+	<h1>Log In!</h1>
+	
+	<?php 
+		// if an error was triggered, show a message
+		if ( $error ) {
+			echo "<div id=\"errormsg\">Username and password do not match. Try again.</div>";
+		}
+	 ?>
+
+
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> 
+		<label for="username">Username</label>
+		<input type="text" name="username" id="username"> <!--"for" should be the same as "id" -->
+		<br />
+
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password"> <!-- type=password displays *** -->
+		<br>
+
+		<input type="submit" value="Log In" id="submit">
+		<input type="hidden" name="did_login" value="1"> <!-- hidden field to ck if user logged in. if did_login = 1, then parse form-->
+	</form>
+
+<?php 
+} // end if not logged in 
+else{ ?>
+	
+	<p id="loggedin">You are logged in!</p>
+	<p><a href="login_cookies_session.php?action=logout">Log Out</a></p> <!-- assigning path and trigger action logout -->
+
+
+<?php }?>
+<?php //echo htmlentities($orig_username) . '<br />' . $clean_username; ?> <!-- shows unclean and clean versions -->
+</div> <!-- end container -->
+</div> <!-- end wrapper -->
+</body>
+</html>
